cc1214ad9932275d5f36b9145dc5e3e776ae4683
tormelo <23294699+tormelo@users.noreply.github.com>
2023-01-27 18:58:53 -0300

    feat: update App

--- app/backend/src/app.ts
+++ app/backend/src/app.ts
@@ -1,4 +1,6 @@
 import * as express from 'express';
+import errorMiddleware from './middlewares/errorMiddleware';
+import LoginRouter from './routes/LoginRouter';
 
 class App {
   public app: express.Express;
@@ -7,9 +9,11 @@ class App {
     this.app = express();
 
     this.config();
+    this.routes();
 
     // Não remover essa rota
     this.app.get('/', (req, res) => res.json({ ok: true }));
+    this.app.use(errorMiddleware);
   }
 
   private config():void {
@@ -22,6 +26,10 @@ class App {
 
     this.app.use(express.json());
     this.app.use(accessControl);
+  }
+
+  private routes():void {
+    this.app.use('/login', LoginRouter);
   }
 
   public start(PORT: string | number):void {
