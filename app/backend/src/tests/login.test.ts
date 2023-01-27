f3b8f43b76565dfbc79bb01ce043f9a1330e7f22
tormelo <23294699+tormelo@users.noreply.github.com>
2023-01-27 19:19:53 -0300

    test: add tests to POST /login

---
+++ app/backend/src/tests/login.test.ts
@@ -0,0 +1,104 @@
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+// @ts-ignore
+import chaiHttp = require('chai-http');
+
+import { app } from '../app';
+import User from '../database/models/User';
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+describe('Para endpoint POST /login', () => {
+  afterEach(() => {
+    sinon.restore();
+  });
+
+  it('deve retornar status 201 em caso de sucesso', async () => {
+    sinon
+      .stub(User, "findOne")
+      .resolves({
+        id: 2,
+        username: 'User',
+        role: 'user',
+        email: 'user@user.com',
+        password: '$2a$08$Y8Abi8jXvsXyqm.rmp0B.uQBA5qUz7T6Ghlg/CvVr/gLxYj5UAZVO'
+      } as User);
+    
+    const httpResponse = await chai
+      .request(app)
+      .post('/login')
+      .send({
+        email: 'user@user.com',
+        password: 'secret_user'
+      })
+    
+    expect(httpResponse.status).to.equal(201);
+    expect(httpResponse.body).to.have.key('token');
+  });
+
+  it('deve retornar status 401 caso email seja inválido', async () => {
+    sinon
+      .stub(User, "findOne")
+      .resolves(null);
+    
+    const httpResponse = await chai
+      .request(app)
+      .post('/login')
+      .send({
+        email: 'invalid-user@user.com',
+        password: 'secret_user'
+      })
+    
+    expect(httpResponse.status).to.equal(401);
+    expect(httpResponse.body).to.deep.equal({message: 'Incorrect email or password'});
+  });
+
+  it('deve retornar status 401 caso senha seja inválida', async () => {
+    sinon
+      .stub(User, "findOne")
+      .resolves({
+        id: 2,
+        username: 'User',
+        role: 'user',
+        email: 'user@user.com',
+        password: '$2a$08$Y8Abi8jXvsXyqm.rmp0B.uQBA5qUz7T6Ghlg/CvVr/gLxYj5UAZVO'
+      } as User);
+    
+    const httpResponse = await chai
+      .request(app)
+      .post('/login')
+      .send({
+        email: 'user@user.com',
+        password: 'invalid_secret_user'
+      })
+    
+    expect(httpResponse.status).to.equal(401);
+    expect(httpResponse.body).to.deep.equal({message: 'Incorrect email or password'});
+  });
+
+  it('deve retornar status 400 caso email não seja informado', async () => {
+    const httpResponse = await chai
+      .request(app)
+      .post('/login')
+      .send({
+        password: 'secret_user'
+      })
+    
+    expect(httpResponse.status).to.equal(400);
+    expect(httpResponse.body).to.deep.equal({message: 'All fields must be filled'});
+  });
+
+  it('deve retornar status 400 caso senha não seja informada', async () => {
+    const httpResponse = await chai
+      .request(app)
+      .post('/login')
+      .send({
+        email: 'user@user.com',
+      })
+    
+    expect(httpResponse.status).to.equal(400);
+    expect(httpResponse.body).to.deep.equal({message: 'All fields must be filled'});
+  });
+});
