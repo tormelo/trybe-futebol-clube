2ee5597559df9d47a93c99064cb4a6b32e39df29
tormelo <23294699+tormelo@users.noreply.github.com>
2023-01-27 18:50:40 -0300

    feat: add LoginController class and login method

---
+++ app/backend/src/controllers/LoginController.ts
@@ -0,0 +1,17 @@
+import { NextFunction, Request, Response } from 'express';
+import LoginService from '../services/LoginService';
+
+class LoginController {
+  constructor(private readonly _service: LoginService) {}
+
+  async login(req: Request, res: Response, next: NextFunction): Promise<Response | void> {
+    try {
+      const response = await this._service.login(req.body);
+      res.status(201).send({ token: response });
+    } catch (error) {
+      next(error);
+    }
+  }
+}
+
+export default LoginController;
