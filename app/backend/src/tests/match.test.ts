8b3a653e4ec3e20236fb5f3655dda2932a91eb1c
tormelo <23294699+tormelo@users.noreply.github.com>
2023-01-29 18:39:16 -0300

    test: add tests to GET /matches inProgress query

--- app/backend/src/tests/match.test.ts
+++ app/backend/src/tests/match.test.ts
@@ -30,4 +30,34 @@ describe('Para endpoint GET /matches', () => {
     expect(httpResponse.status).to.equal(200);
     expect(httpResponse.body).to.deep.equal(unfilteredMatches);
   });
+
+  it('com query inProgress=true deve retornar status 200 e partidas em andamento', async () => {
+    const filteredMatches = unfilteredMatches.filter(({inProgress}) => inProgress === true);
+    
+    sinon
+      .stub(Match, "findAll")
+      .resolves(filteredMatches as unknown as Match[]);
+    
+    const httpResponse = await chai
+      .request(app)
+      .get('/matches?inProgress=true')
+    
+    expect(httpResponse.status).to.equal(200);
+    expect(httpResponse.body).to.deep.equal(filteredMatches);
+  });
+
+  it('com query inProgress=false deve retornar status 200 e partidas em andamento', async () => {
+    const filteredMatches = unfilteredMatches.filter(({inProgress}) => inProgress === false);
+    
+    sinon
+      .stub(Match, "findAll")
+      .resolves(filteredMatches as unknown as Match[]);
+    
+    const httpResponse = await chai
+      .request(app)
+      .get('/matches?inProgress=false')
+    
+    expect(httpResponse.status).to.equal(200);
+    expect(httpResponse.body).to.deep.equal(filteredMatches);
+  });
 });
