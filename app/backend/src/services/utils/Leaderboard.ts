00336fdd09890aafed64f2ecaa39674b8da0bb35
tormelo <23294699+tormelo@users.noreply.github.com>
2023-01-30 13:37:56 -0300

    feat: add getEfficiency method to Leaderboard class

--- app/backend/src/services/utils/Leaderboard.ts
+++ app/backend/src/services/utils/Leaderboard.ts
@@ -13,7 +13,6 @@ class Leaderboard {
       totalLosses: 0,
     };
 
-    console.log(matches);
     matches.forEach(({ goalsFavor, goalsOwn }) => {
       if (goalsFavor > goalsOwn) results.totalVictories += 1;
       if (goalsFavor < goalsOwn) results.totalLosses += 1;
@@ -26,6 +25,10 @@ class Leaderboard {
   private static getPoints(results: ILeaderboardResults): number {
     const { totalVictories, totalDraws } = results;
     return (totalVictories * 3) + totalDraws;
+  }
+
+  private static getEfficiency(totalPoints: number, totalGames: number): string {
+    return ((totalPoints / (totalGames * 3)) * 100).toFixed(2);
   }
 }
 
