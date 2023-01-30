be2268b70152b0989e1df81f3437522b3105ad6e
tormelo <23294699+tormelo@users.noreply.github.com>
2023-01-30 14:20:30 -0300

    feat: add LeaderboardController class and getHomeLeaderboard method

---
+++ app/backend/src/controllers/LeaderboardController.ts
@@ -0,0 +1,19 @@
+import { NextFunction, Request, Response } from 'express';
+import LeaderboardService from '../services/LeaderboardService';
+
+class LeaderboardController {
+  static async getHomeLeaderboard(
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ): Promise<Response | void> {
+    try {
+      const response = await LeaderboardService.getHomeLeaderboard();
+      res.status(200).send(response);
+    } catch (error) {
+      next(error);
+    }
+  }
+}
+
+export default LeaderboardController;
