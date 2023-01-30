39dacc1a35404b63f87360de549b308d1c4c0269
tormelo <23294699+tormelo@users.noreply.github.com>
2023-01-30 15:31:59 -0300

    test: add tests to GET /leaderboard/away endpoint

--- app/backend/src/tests/leaderboard.test.ts
+++ app/backend/src/tests/leaderboard.test.ts
@@ -5,7 +5,12 @@ import chaiHttp = require('chai-http');
 
 import { app } from '../app';
 import Team from '../database/models/Team';
-import { homeLeaderboard, homeTeamMatches } from './mocks/leaderboard.mock';
+import { 
+  awayLeaderboardMock,
+  awayMatchesMock,
+  homeLeaderboardMock,
+  homeMatchesMock 
+} from './mocks/leaderboard.mock';
 
 chai.use(chaiHttp);
 
@@ -19,13 +24,32 @@ describe('Para endpoint GET /leaderboard/home', () => {
   it('deve retornar status 200 e leaderboard em caso de sucesso', async () => {
     sinon
       .stub(Team, "findAll")
-      .resolves(homeTeamMatches as unknown as Team[]);
+      .resolves(homeMatchesMock as unknown as Team[]);
     
     const httpResponse = await chai
       .request(app)
       .get('/leaderboard/home')
     
     expect(httpResponse.status).to.equal(200);
-    expect(httpResponse.body).to.deep.equal(homeLeaderboard);
+    expect(httpResponse.body).to.deep.equal(homeLeaderboardMock);
+  });
+});
+
+describe('Para endpoint GET /leaderboard/away', () => {
+  afterEach(() => {
+    sinon.restore();
+  });
+
+  it('deve retornar status 200 e leaderboard em caso de sucesso', async () => {
+    sinon
+      .stub(Team, "findAll")
+      .resolves(awayMatchesMock as unknown as Team[]);
+    
+    const httpResponse = await chai
+      .request(app)
+      .get('/leaderboard/away')
+    
+    expect(httpResponse.status).to.equal(200);
+    expect(httpResponse.body).to.deep.equal(awayLeaderboardMock);
   });
 });
--- app/backend/src/tests/mocks/leaderboard.mock.ts
+++ app/backend/src/tests/mocks/leaderboard.mock.ts
@@ -1,4 +1,4 @@
-export const homeTeamMatches = [
+export const homeMatchesMock = [
   {
     "teamName": "São Paulo",
     "homeMatches": [
@@ -245,7 +245,254 @@ export const homeTeamMatches = [
   }
 ]
 
-export const homeLeaderboard = [
+export const awayMatchesMock = [
+  {
+    "teamName": "Grêmio",
+    "awayMatches": [
+      {
+        "goalsFavor": 1,
+        "goalsOwn": 1
+      },
+      {
+        "goalsFavor": 3,
+        "goalsOwn": 2
+      },
+      {
+        "goalsFavor": 1,
+        "goalsOwn": 4
+      }
+    ]
+  },
+  {
+    "teamName": "Santos",
+    "awayMatches": [
+      {
+        "goalsFavor": 1,
+        "goalsOwn": 1
+      },
+      {
+        "goalsFavor": 2,
+        "goalsOwn": 2
+      }
+    ]
+  },
+  {
+    "teamName": "Napoli-SC",
+    "awayMatches": [
+      {
+        "goalsFavor": 0,
+        "goalsOwn": 3
+      },
+      {
+        "goalsFavor": 1,
+        "goalsOwn": 5
+      },
+      {
+        "goalsFavor": 0,
+        "goalsOwn": 2
+      }
+    ]
+  },
+  {
+    "teamName": "Bahia",
+    "awayMatches": [
+      {
+        "goalsFavor": 0,
+        "goalsOwn": 0
+      },
+      {
+        "goalsFavor": 2,
+        "goalsOwn": 2
+      }
+    ]
+  },
+  {
+    "teamName": "Minas Brasília",
+    "awayMatches": [
+      {
+        "goalsFavor": 1,
+        "goalsOwn": 1
+      },
+      {
+        "goalsFavor": 0,
+        "goalsOwn": 2
+      }
+    ]
+  },
+  {
+    "teamName": "Real Brasília",
+    "awayMatches": [
+      {
+        "goalsFavor": 1,
+        "goalsOwn": 1
+      },
+      {
+        "goalsFavor": 1,
+        "goalsOwn": 3
+      },
+      {
+        "goalsFavor": 1,
+        "goalsOwn": 0
+      }
+    ]
+  },
+  {
+    "teamName": "Ferroviária",
+    "awayMatches": [
+      {
+        "goalsFavor": 2,
+        "goalsOwn": 2
+      },
+      {
+        "goalsFavor": 1,
+        "goalsOwn": 0
+      },
+      {
+        "goalsFavor": 1,
+        "goalsOwn": 3
+      }
+    ]
+  },
+  {
+    "teamName": "Avaí/Kindermann",
+    "awayMatches": [
+      {
+        "goalsFavor": 1,
+        "goalsOwn": 0
+      },
+      {
+        "goalsFavor": 0,
+        "goalsOwn": 1
+      }
+    ]
+  },
+  {
+    "teamName": "Palmeiras",
+    "awayMatches": [
+      {
+        "goalsFavor": 3,
+        "goalsOwn": 0
+      },
+      {
+        "goalsFavor": 4,
+        "goalsOwn": 0
+      }
+    ]
+  },
+  {
+    "teamName": "Internacional",
+    "awayMatches": [
+      {
+        "goalsFavor": 2,
+        "goalsOwn": 0
+      },
+      {
+        "goalsFavor": 1,
+        "goalsOwn": 0
+      },
+      {
+        "goalsFavor": 1,
+        "goalsOwn": 2
+      }
+    ]
+  },
+  {
+    "teamName": "Botafogo",
+    "awayMatches": [
+      {
+        "goalsFavor": 0,
+        "goalsOwn": 1
+      },
+      {
+        "goalsFavor": 1,
+        "goalsOwn": 3
+      }
+    ]
+  },
+  {
+    "teamName": "Corinthians",
+    "awayMatches": [
+      {
+        "goalsFavor": 1,
+        "goalsOwn": 0
+      },
+      {
+        "goalsFavor": 4,
+        "goalsOwn": 0
+      },
+      {
+        "goalsFavor": 1,
+        "goalsOwn": 2
+      }
+    ]
+  },
+  {
+    "teamName": "Cruzeiro",
+    "awayMatches": [
+      {
+        "goalsFavor": 1,
+        "goalsOwn": 2
+      },
+      {
+        "goalsFavor": 2,
+        "goalsOwn": 4
+      },
+      {
+        "goalsFavor": 3,
+        "goalsOwn": 1
+      }
+    ]
+  },
+  {
+    "teamName": "São Paulo",
+    "awayMatches": [
+      {
+        "goalsFavor": 1,
+        "goalsOwn": 2
+      },
+      {
+        "goalsFavor": 3,
+        "goalsOwn": 2
+      },
+      {
+        "goalsFavor": 1,
+        "goalsOwn": 1
+      }
+    ]
+  },
+  {
+    "teamName": "São José-SP",
+    "awayMatches": [
+      {
+        "goalsFavor": 1,
+        "goalsOwn": 0
+      },
+      {
+        "goalsFavor": 2,
+        "goalsOwn": 1
+      }
+    ]
+  },
+  {
+    "teamName": "Flamengo",
+    "awayMatches": [
+      {
+        "goalsFavor": 0,
+        "goalsOwn": 0
+      },
+      {
+        "goalsFavor": 0,
+        "goalsOwn": 3
+      },
+      {
+        "goalsFavor": 1,
+        "goalsOwn": 0
+      }
+    ]
+  }
+]
+
+export const homeLeaderboardMock = [
   {
     "name": "Santos",
     "totalPoints": 9,
@@ -436,6 +683,201 @@ export const homeLeaderboard = [
     "goalsFavor": 0,
     "goalsOwn": 4,
     "goalsBalance": -4,
+    "efficiency": "0.00"
+  }
+]
+
+export const awayLeaderboardMock = [
+  {
+    "name": "Palmeiras",
+    "totalPoints": 6,
+    "totalGames": 2,
+    "totalVictories": 2,
+    "totalDraws": 0,
+    "totalLosses": 0,
+    "goalsFavor": 7,
+    "goalsOwn": 0,
+    "goalsBalance": 7,
+    "efficiency": "100.00"
+  },
+  {
+    "name": "Corinthians",
+    "totalPoints": 6,
+    "totalGames": 3,
+    "totalVictories": 2,
+    "totalDraws": 0,
+    "totalLosses": 1,
+    "goalsFavor": 6,
+    "goalsOwn": 2,
+    "goalsBalance": 4,
+    "efficiency": "66.67"
+  },
+  {
+    "name": "Internacional",
+    "totalPoints": 6,
+    "totalGames": 3,
+    "totalVictories": 2,
+    "totalDraws": 0,
+    "totalLosses": 1,
+    "goalsFavor": 4,
+    "goalsOwn": 2,
+    "goalsBalance": 2,
+    "efficiency": "66.67"
+  },
+  {
+    "name": "São José-SP",
+    "totalPoints": 6,
+    "totalGames": 2,
+    "totalVictories": 2,
+    "totalDraws": 0,
+    "totalLosses": 0,
+    "goalsFavor": 3,
+    "goalsOwn": 1,
+    "goalsBalance": 2,
+    "efficiency": "100.00"
+  },
+  {
+    "name": "São Paulo",
+    "totalPoints": 4,
+    "totalGames": 3,
+    "totalVictories": 1,
+    "totalDraws": 1,
+    "totalLosses": 1,
+    "goalsFavor": 5,
+    "goalsOwn": 5,
+    "goalsBalance": 0,
+    "efficiency": "44.44"
+  },
+  {
+    "name": "Ferroviária",
+    "totalPoints": 4,
+    "totalGames": 3,
+    "totalVictories": 1,
+    "totalDraws": 1,
+    "totalLosses": 1,
+    "goalsFavor": 4,
+    "goalsOwn": 5,
+    "goalsBalance": -1,
+    "efficiency": "44.44"
+  },
+  {
+    "name": "Real Brasília",
+    "totalPoints": 4,
+    "totalGames": 3,
+    "totalVictories": 1,
+    "totalDraws": 1,
+    "totalLosses": 1,
+    "goalsFavor": 3,
+    "goalsOwn": 4,
+    "goalsBalance": -1,
+    "efficiency": "44.44"
+  },
+  {
+    "name": "Grêmio",
+    "totalPoints": 4,
+    "totalGames": 3,
+    "totalVictories": 1,
+    "totalDraws": 1,
+    "totalLosses": 1,
+    "goalsFavor": 5,
+    "goalsOwn": 7,
+    "goalsBalance": -2,
+    "efficiency": "44.44"
+  },
+  {
+    "name": "Flamengo",
+    "totalPoints": 4,
+    "totalGames": 3,
+    "totalVictories": 1,
+    "totalDraws": 1,
+    "totalLosses": 1,
+    "goalsFavor": 1,
+    "goalsOwn": 3,
+    "goalsBalance": -2,
+    "efficiency": "44.44"
+  },
+  {
+    "name": "Avaí/Kindermann",
+    "totalPoints": 3,
+    "totalGames": 2,
+    "totalVictories": 1,
+    "totalDraws": 0,
+    "totalLosses": 1,
+    "goalsFavor": 1,
+    "goalsOwn": 1,
+    "goalsBalance": 0,
+    "efficiency": "50.00"
+  },
+  {
+    "name": "Cruzeiro",
+    "totalPoints": 3,
+    "totalGames": 3,
+    "totalVictories": 1,
+    "totalDraws": 0,
+    "totalLosses": 2,
+    "goalsFavor": 6,
+    "goalsOwn": 7,
+    "goalsBalance": -1,
+    "efficiency": "33.33"
+  },
+  {
+    "name": "Santos",
+    "totalPoints": 2,
+    "totalGames": 2,
+    "totalVictories": 0,
+    "totalDraws": 2,
+    "totalLosses": 0,
+    "goalsFavor": 3,
+    "goalsOwn": 3,
+    "goalsBalance": 0,
+    "efficiency": "33.33"
+  },
+  {
+    "name": "Bahia",
+    "totalPoints": 2,
+    "totalGames": 2,
+    "totalVictories": 0,
+    "totalDraws": 2,
+    "totalLosses": 0,
+    "goalsFavor": 2,
+    "goalsOwn": 2,
+    "goalsBalance": 0,
+    "efficiency": "33.33"
+  },
+  {
+    "name": "Minas Brasília",
+    "totalPoints": 1,
+    "totalGames": 2,
+    "totalVictories": 0,
+    "totalDraws": 1,
+    "totalLosses": 1,
+    "goalsFavor": 1,
+    "goalsOwn": 3,
+    "goalsBalance": -2,
+    "efficiency": "16.67"
+  },
+  {
+    "name": "Botafogo",
+    "totalPoints": 0,
+    "totalGames": 2,
+    "totalVictories": 0,
+    "totalDraws": 0,
+    "totalLosses": 2,
+    "goalsFavor": 1,
+    "goalsOwn": 4,
+    "goalsBalance": -3,
+    "efficiency": "0.00"
+  },
+  {
+    "name": "Napoli-SC",
+    "totalPoints": 0,
+    "totalGames": 3,
+    "totalVictories": 0,
+    "totalDraws": 0,
+    "totalLosses": 3,
+    "goalsFavor": 1,
+    "goalsOwn": 10,
+    "goalsBalance": -9,
     "efficiency": "0.00"
   }
 ]
