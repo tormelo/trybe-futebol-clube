9a305f185b2bf0ac5ddd2f09a0fa6d966ce1ee1a
tormelo <23294699+tormelo@users.noreply.github.com>
2023-01-30 16:18:39 -0300

    test: add tests to GET /leaderboard endpoint

--- app/backend/src/tests/leaderboard.test.ts
+++ app/backend/src/tests/leaderboard.test.ts
@@ -6,10 +6,12 @@ import chaiHttp = require('chai-http');
 import { app } from '../app';
 import Team from '../database/models/Team';
 import { 
+  allMatchesMock,
   awayLeaderboardMock,
   awayMatchesMock,
   homeLeaderboardMock,
-  homeMatchesMock 
+  homeMatchesMock, 
+  leaderboardMock
 } from './mocks/leaderboard.mock';
 
 chai.use(chaiHttp);
@@ -51,5 +53,24 @@ describe('Para endpoint GET /leaderboard/away', () => {
     
     expect(httpResponse.status).to.equal(200);
     expect(httpResponse.body).to.deep.equal(awayLeaderboardMock);
+  });
+});
+
+describe('Para endpoint GET /leaderboard', () => {
+  afterEach(() => {
+    sinon.restore();
+  });
+
+  it('deve retornar status 200 e leaderboard em caso de sucesso', async () => {
+    sinon
+      .stub(Team, "findAll")
+      .resolves(allMatchesMock as unknown as Team[]);
+    
+    const httpResponse = await chai
+      .request(app)
+      .get('/leaderboard')
+    
+    expect(httpResponse.status).to.equal(200);
+    expect(httpResponse.body).to.deep.equal(leaderboardMock);
   });
 });
--- app/backend/src/tests/mocks/leaderboard.mock.ts
+++ app/backend/src/tests/mocks/leaderboard.mock.ts
@@ -492,6 +492,449 @@ export const awayMatchesMock = [
   }
 ]
 
+export const allMatchesMock = [
+  {
+    "teamName": "São Paulo",
+    "homeMatches": [
+      {
+        "goalsFavor": 1,
+        "goalsOwn": 1
+      },
+      {
+        "goalsFavor": 3,
+        "goalsOwn": 0
+      }
+    ],
+    "awayMatches": [
+      {
+        "goalsFavor": 1,
+        "goalsOwn": 2
+      },
+      {
+        "goalsFavor": 3,
+        "goalsOwn": 2
+      },
+      {
+        "goalsFavor": 1,
+        "goalsOwn": 1
+      }
+    ]
+  },
+  {
+    "teamName": "Internacional",
+    "homeMatches": [
+      {
+        "goalsFavor": 1,
+        "goalsOwn": 1
+      },
+      {
+        "goalsFavor": 0,
+        "goalsOwn": 4
+      },
+      {
+        "goalsFavor": 3,
+        "goalsOwn": 1
+      }
+    ],
+    "awayMatches": [
+      {
+        "goalsFavor": 2,
+        "goalsOwn": 0
+      },
+      {
+        "goalsFavor": 1,
+        "goalsOwn": 0
+      }
+    ]
+  },
+  {
+    "teamName": "Corinthians",
+    "homeMatches": [
+      {
+        "goalsFavor": 3,
+        "goalsOwn": 0
+      },
+      {
+        "goalsFavor": 3,
+        "goalsOwn": 1
+      }
+    ],
+    "awayMatches": [
+      {
+        "goalsFavor": 1,
+        "goalsOwn": 0
+      },
+      {
+        "goalsFavor": 4,
+        "goalsOwn": 0
+      },
+      {
+        "goalsFavor": 1,
+        "goalsOwn": 2
+      }
+    ]
+  },
+  {
+    "teamName": "Botafogo",
+    "homeMatches": [
+      {
+        "goalsFavor": 0,
+        "goalsOwn": 0
+      },
+      {
+        "goalsFavor": 0,
+        "goalsOwn": 4
+      },
+      {
+        "goalsFavor": 2,
+        "goalsOwn": 0
+      }
+    ],
+    "awayMatches": [
+      {
+        "goalsFavor": 0,
+        "goalsOwn": 1
+      },
+      {
+        "goalsFavor": 1,
+        "goalsOwn": 3
+      }
+    ]
+  },
+  {
+    "teamName": "Flamengo",
+    "homeMatches": [
+      {
+        "goalsFavor": 1,
+        "goalsOwn": 1
+      },
+      {
+        "goalsFavor": 0,
+        "goalsOwn": 1
+      }
+    ],
+    "awayMatches": [
+      {
+        "goalsFavor": 0,
+        "goalsOwn": 0
+      },
+      {
+        "goalsFavor": 0,
+        "goalsOwn": 3
+      },
+      {
+        "goalsFavor": 1,
+        "goalsOwn": 0
+      }
+    ]
+  },
+  {
+    "teamName": "Cruzeiro",
+    "homeMatches": [
+      {
+        "goalsFavor": 1,
+        "goalsOwn": 1
+      },
+      {
+        "goalsFavor": 1,
+        "goalsOwn": 2
+      }
+    ],
+    "awayMatches": [
+      {
+        "goalsFavor": 1,
+        "goalsOwn": 2
+      },
+      {
+        "goalsFavor": 2,
+        "goalsOwn": 4
+      },
+      {
+        "goalsFavor": 3,
+        "goalsOwn": 1
+      }
+    ]
+  },
+  {
+    "teamName": "Palmeiras",
+    "homeMatches": [
+      {
+        "goalsFavor": 2,
+        "goalsOwn": 2
+      },
+      {
+        "goalsFavor": 4,
+        "goalsOwn": 2
+      },
+      {
+        "goalsFavor": 4,
+        "goalsOwn": 1
+      }
+    ],
+    "awayMatches": [
+      {
+        "goalsFavor": 3,
+        "goalsOwn": 0
+      },
+      {
+        "goalsFavor": 4,
+        "goalsOwn": 0
+      }
+    ]
+  },
+  {
+    "teamName": "São José-SP",
+    "homeMatches": [
+      {
+        "goalsFavor": 0,
+        "goalsOwn": 1
+      },
+      {
+        "goalsFavor": 2,
+        "goalsOwn": 3
+      },
+      {
+        "goalsFavor": 0,
+        "goalsOwn": 1
+      }
+    ],
+    "awayMatches": [
+      {
+        "goalsFavor": 1,
+        "goalsOwn": 0
+      },
+      {
+        "goalsFavor": 2,
+        "goalsOwn": 1
+      }
+    ]
+  },
+  {
+    "teamName": "Avaí/Kindermann",
+    "homeMatches": [
+      {
+        "goalsFavor": 0,
+        "goalsOwn": 3
+      },
+      {
+        "goalsFavor": 2,
+        "goalsOwn": 3
+      },
+      {
+        "goalsFavor": 1,
+        "goalsOwn": 1
+      }
+    ],
+    "awayMatches": [
+      {
+        "goalsFavor": 1,
+        "goalsOwn": 0
+      },
+      {
+        "goalsFavor": 0,
+        "goalsOwn": 1
+      }
+    ]
+  },
+  {
+    "teamName": "Bahia",
+    "homeMatches": [
+      {
+        "goalsFavor": 0,
+        "goalsOwn": 2
+      },
+      {
+        "goalsFavor": 0,
+        "goalsOwn": 1
+      },
+      {
+        "goalsFavor": 0,
+        "goalsOwn": 1
+      }
+    ],
+    "awayMatches": [
+      {
+        "goalsFavor": 0,
+        "goalsOwn": 0
+      },
+      {
+        "goalsFavor": 2,
+        "goalsOwn": 2
+      }
+    ]
+  },
+  {
+    "teamName": "Real Brasília",
+    "homeMatches": [
+      {
+        "goalsFavor": 1,
+        "goalsOwn": 0
+      },
+      {
+        "goalsFavor": 1,
+        "goalsOwn": 0
+      }
+    ],
+    "awayMatches": [
+      {
+        "goalsFavor": 1,
+        "goalsOwn": 1
+      },
+      {
+        "goalsFavor": 1,
+        "goalsOwn": 3
+      },
+      {
+        "goalsFavor": 1,
+        "goalsOwn": 0
+      }
+    ]
+  },
+  {
+    "teamName": "Ferroviária",
+    "homeMatches": [
+      {
+        "goalsFavor": 0,
+        "goalsOwn": 1
+      },
+      {
+        "goalsFavor": 3,
+        "goalsOwn": 1
+      }
+    ],
+    "awayMatches": [
+      {
+        "goalsFavor": 2,
+        "goalsOwn": 2
+      },
+      {
+        "goalsFavor": 1,
+        "goalsOwn": 0
+      },
+      {
+        "goalsFavor": 1,
+        "goalsOwn": 3
+      },
+      {
+        "goalsFavor": 0,
+        "goalsOwn": 1
+      }
+    ]
+  },
+  {
+    "teamName": "Grêmio",
+    "homeMatches": [
+      {
+        "goalsFavor": 2,
+        "goalsOwn": 1
+      },
+      {
+        "goalsFavor": 2,
+        "goalsOwn": 0
+      }
+    ],
+    "awayMatches": [
+      {
+        "goalsFavor": 1,
+        "goalsOwn": 1
+      },
+      {
+        "goalsFavor": 3,
+        "goalsOwn": 2
+      },
+      {
+        "goalsFavor": 1,
+        "goalsOwn": 4
+      }
+    ]
+  },
+  {
+    "teamName": "Santos",
+    "homeMatches": [
+      {
+        "goalsFavor": 2,
+        "goalsOwn": 1
+      },
+      {
+        "goalsFavor": 5,
+        "goalsOwn": 1
+      },
+      {
+        "goalsFavor": 2,
+        "goalsOwn": 1
+      }
+    ],
+    "awayMatches": [
+      {
+        "goalsFavor": 1,
+        "goalsOwn": 1
+      },
+      {
+        "goalsFavor": 2,
+        "goalsOwn": 2
+      }
+    ]
+  },
+  {
+    "teamName": "Minas Brasília",
+    "homeMatches": [
+      {
+        "goalsFavor": 0,
+        "goalsOwn": 1
+      },
+      {
+        "goalsFavor": 2,
+        "goalsOwn": 2
+      },
+      {
+        "goalsFavor": 1,
+        "goalsOwn": 3
+      },
+      {
+        "goalsFavor": 1,
+        "goalsOwn": 0
+      }
+    ],
+    "awayMatches": [
+      {
+        "goalsFavor": 1,
+        "goalsOwn": 1
+      },
+      {
+        "goalsFavor": 0,
+        "goalsOwn": 2
+      }
+    ]
+  },
+  {
+    "teamName": "Napoli-SC",
+    "homeMatches": [
+      {
+        "goalsFavor": 0,
+        "goalsOwn": 0
+      },
+      {
+        "goalsFavor": 2,
+        "goalsOwn": 2
+      }
+    ],
+    "awayMatches": [
+      {
+        "goalsFavor": 0,
+        "goalsOwn": 3
+      },
+      {
+        "goalsFavor": 1,
+        "goalsOwn": 5
+      },
+      {
+        "goalsFavor": 0,
+        "goalsOwn": 2
+      }
+    ]
+  }
+]
+
 export const homeLeaderboardMock = [
   {
     "name": "Santos",
@@ -879,5 +1322,200 @@ export const awayLeaderboardMock = [
     "goalsOwn": 10,
     "goalsBalance": -9,
     "efficiency": "0.00"
+  }
+]
+
+export const leaderboardMock = [
+  {
+    "name": "Palmeiras",
+    "totalPoints": 13,
+    "totalGames": 5,
+    "totalVictories": 4,
+    "totalDraws": 1,
+    "totalLosses": 0,
+    "goalsFavor": 17,
+    "goalsOwn": 5,
+    "goalsBalance": 12,
+    "efficiency": "86.67"
+  },
+  {
+    "name": "Corinthians",
+    "totalPoints": 12,
+    "totalGames": 5,
+    "totalVictories": 4,
+    "totalDraws": 0,
+    "totalLosses": 1,
+    "goalsFavor": 12,
+    "goalsOwn": 3,
+    "goalsBalance": 9,
+    "efficiency": "80.00"
+  },
+  {
+    "name": "Santos",
+    "totalPoints": 11,
+    "totalGames": 5,
+    "totalVictories": 3,
+    "totalDraws": 2,
+    "totalLosses": 0,
+    "goalsFavor": 12,
+    "goalsOwn": 6,
+    "goalsBalance": 6,
+    "efficiency": "73.33"
+  },
+  {
+    "name": "Grêmio",
+    "totalPoints": 10,
+    "totalGames": 5,
+    "totalVictories": 3,
+    "totalDraws": 1,
+    "totalLosses": 1,
+    "goalsFavor": 9,
+    "goalsOwn": 8,
+    "goalsBalance": 1,
+    "efficiency": "66.67"
+  },
+  {
+    "name": "Internacional",
+    "totalPoints": 10,
+    "totalGames": 5,
+    "totalVictories": 3,
+    "totalDraws": 1,
+    "totalLosses": 1,
+    "goalsFavor": 7,
+    "goalsOwn": 6,
+    "goalsBalance": 1,
+    "efficiency": "66.67"
+  },
+  {
+    "name": "Real Brasília",
+    "totalPoints": 10,
+    "totalGames": 5,
+    "totalVictories": 3,
+    "totalDraws": 1,
+    "totalLosses": 1,
+    "goalsFavor": 5,
+    "goalsOwn": 4,
+    "goalsBalance": 1,
+    "efficiency": "66.67"
+  },
+  {
+    "name": "São Paulo",
+    "totalPoints": 8,
+    "totalGames": 5,
+    "totalVictories": 2,
+    "totalDraws": 2,
+    "totalLosses": 1,
+    "goalsFavor": 9,
+    "goalsOwn": 6,
+    "goalsBalance": 3,
+    "efficiency": "53.33"
+  },
+  {
+    "name": "Ferroviária",
+    "totalPoints": 7,
+    "totalGames": 6,
+    "totalVictories": 2,
+    "totalDraws": 1,
+    "totalLosses": 3,
+    "goalsFavor": 7,
+    "goalsOwn": 8,
+    "goalsBalance": -1,
+    "efficiency": "38.89"
+  },
+  {
+    "name": "São José-SP",
+    "totalPoints": 6,
+    "totalGames": 5,
+    "totalVictories": 2,
+    "totalDraws": 0,
+    "totalLosses": 3,
+    "goalsFavor": 5,
+    "goalsOwn": 6,
+    "goalsBalance": -1,
+    "efficiency": "40.00"
+  },
+  {
+    "name": "Flamengo",
+    "totalPoints": 5,
+    "totalGames": 5,
+    "totalVictories": 1,
+    "totalDraws": 2,
+    "totalLosses": 2,
+    "goalsFavor": 2,
+    "goalsOwn": 5,
+    "goalsBalance": -3,
+    "efficiency": "33.33"
+  },
+  {
+    "name": "Minas Brasília",
+    "totalPoints": 5,
+    "totalGames": 6,
+    "totalVictories": 1,
+    "totalDraws": 2,
+    "totalLosses": 3,
+    "goalsFavor": 5,
+    "goalsOwn": 9,
+    "goalsBalance": -4,
+    "efficiency": "27.78"
+  },
+  {
+    "name": "Cruzeiro",
+    "totalPoints": 4,
+    "totalGames": 5,
+    "totalVictories": 1,
+    "totalDraws": 1,
+    "totalLosses": 3,
+    "goalsFavor": 8,
+    "goalsOwn": 10,
+    "goalsBalance": -2,
+    "efficiency": "26.67"
+  },
+  {
+    "name": "Avaí/Kindermann",
+    "totalPoints": 4,
+    "totalGames": 5,
+    "totalVictories": 1,
+    "totalDraws": 1,
+    "totalLosses": 3,
+    "goalsFavor": 4,
+    "goalsOwn": 8,
+    "goalsBalance": -4,
+    "efficiency": "26.67"
+  },
+  {
+    "name": "Botafogo",
+    "totalPoints": 4,
+    "totalGames": 5,
+    "totalVictories": 1,
+    "totalDraws": 1,
+    "totalLosses": 3,
+    "goalsFavor": 3,
+    "goalsOwn": 8,
+    "goalsBalance": -5,
+    "efficiency": "26.67"
+  },
+  {
+    "name": "Bahia",
+    "totalPoints": 2,
+    "totalGames": 5,
+    "totalVictories": 0,
+    "totalDraws": 2,
+    "totalLosses": 3,
+    "goalsFavor": 2,
+    "goalsOwn": 6,
+    "goalsBalance": -4,
+    "efficiency": "13.33"
+  },
+  {
+    "name": "Napoli-SC",
+    "totalPoints": 2,
+    "totalGames": 5,
+    "totalVictories": 0,
+    "totalDraws": 2,
+    "totalLosses": 3,
+    "goalsFavor": 3,
+    "goalsOwn": 12,
+    "goalsBalance": -9,
+    "efficiency": "13.33"
   }
 ]
