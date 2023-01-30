32b4dc0b48522805f0ea18a5c98a00af42dff292
tormelo <23294699+tormelo@users.noreply.github.com>
2023-01-30 15:26:27 -0300

    feat: add getAwayLeaderboard method to LeaderboardService class

--- app/backend/src/services/LeaderboardService.ts
+++ app/backend/src/services/LeaderboardService.ts
@@ -22,6 +22,24 @@ class LeaderboardService {
 
     return leaderboard;
   }
+
+  static async getAwayLeaderboard(): Promise<ILeaderboardTeam[]> {
+    const teams = await Team.findAll({
+      attributes: [['team_name', 'teamName']],
+      include: [{
+        model: Match,
+        as: 'awayMatches',
+        attributes: [['away_team_goals', 'goalsFavor'], ['home_team_goals', 'goalsOwn']],
+        where: { inProgress: false } }],
+    });
+
+    const teamsJSON = JSON.stringify(teams);
+    const teamMatches = JSON.parse(teamsJSON) as ILeaderboardModel[];
+
+    const leaderboard = Leaderboard.getAwayLeaderboard(teamMatches);
+
+    return leaderboard;
+  }
 }
 
 export default LeaderboardService;
